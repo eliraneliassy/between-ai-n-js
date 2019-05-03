f86e0993cd3b8e723d833c236c13259c71a09948
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-05-03 16:02:41 +0300

    code cleanup

--- src/app/image-classfier-upload/image-classfier-upload.component.ts
+++ src/app/image-classfier-upload/image-classfier-upload.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Prediction } from '../prediction';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-image-classfier-upload',
@@ -8,40 +7,12 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
   styleUrls: ['./image-classfier-upload.component.scss']
 })
 export class ImageClassfierUploadComponent implements OnInit {
-  imageSrc: string;
-  @ViewChild('img') imageEl: ElementRef;
-
-  predictions: Prediction[];
-
-  model: any;
-  loading = true;
-
 
   constructor() { }
 
-  async ngOnInit() {
-    console.log('loading mobilenet model...');
-    this.model = await mobilenet.load();
-    console.log('Sucessfully loaded model');
-    this.loading = false;
-  }
-
-  async fileChangeEvent(event) {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-
-      reader.readAsDataURL(event.target.files[0]);
-
-      reader.onload = (res: any) => {
-        this.imageSrc = res.target.result;
-        setTimeout(async () => {
-          const imgEl = this.imageEl.nativeElement;
-          this.predictions = await this.model.classify(imgEl);
-        }, 0);
-
-      };
-    }
+  ngOnInit() {
 
   }
+
 
 }
--- src/app/image-classifier-webcam/image-classifier-webcam.component.ts
+++ src/app/image-classifier-webcam/image-classifier-webcam.component.ts
@@ -1,46 +1,21 @@
-import { Prediction } from './../prediction';
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import * as mobilenet from '@tensorflow-models/mobilenet';
-import * as tf from '@tensorflow/tfjs';
+
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-image-classifier-webcam',
   templateUrl: './image-classifier-webcam.component.html',
   styleUrls: ['./image-classifier-webcam.component.scss']
 })
-export class ImageClassifierWebcamComponent implements OnInit, AfterViewInit {
+export class ImageClassifierWebcamComponent implements OnInit {
 
-  @ViewChild('video') video: ElementRef;
-  predictions: Prediction[];
-  model: any;
-  loading = true;
+
   constructor() { }
 
-  async ngOnInit() {
-    console.log('loading mobilenet model...');
-    this.model = await mobilenet.load();
-    console.log('Sucessfully loaded model');
-    this.loading = false;
+  ngOnInit() {
 
-    setInterval(async () => {
-      this.predictions = await this.model.classify(this.video.nativeElement);
-      await tf.nextFrame();
-    }, 3000);
   }
 
-  async ngAfterViewInit() {
-    const vid = this.video.nativeElement;
 
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-          vid.srcObject = stream;
-
-        })
-        .catch((err0r) => {
-          console.log('Something went wrong!');
-        });
-    }
-  }
 
 }
